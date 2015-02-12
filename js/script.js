1411ce2241901f684189284f27825bb5327eb231
Michkkael <Michkkael@gmail.com>
2015-02-12 20:38:34 +0100

    Intégration du header (bureau)
    Début de l'intégration du header, en particulier le h1 contenant le
    logo, les liens mails et facebook et le nav de langue

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 	$('#nav>ul').hide();
 
-	$('#triggerMenu').click(function(event) {
+	$('#triggerMenu').click(function (event) {
 		$('#nav>ul').stop().slideToggle(500,
-			function(){
+			function () {
+				//TODO : Corriger le bug du triangle quand on double clique sur MENU
 				$('#nav>ul').toggleClass('before');
 			}
 		);
 	});
 
 	var $bubble = $('a.bubble i');
-
 	$bubble.css({
 		'height': $bubble.width(),
-		'line-height': $bubble.width()+'px'
+		'line-height': $bubble.width() + 'px'
 	});
+
+	$(window).resize(function ($bubble) {
+		var $bubble = $('a.bubble i');
+		$bubble.css({
+			'height': $bubble.width(),
+			'line-height': $bubble.width() + 'px'
+		});
+	});
+
 
 	var $ensoleillement = $('#meteo-section .ensoleillement');
 	var $temperature = $('#meteo-section .temperature');
 
 	$temperature.css({
 		'height': $ensoleillement.height(),
-		'line-height': $ensoleillement.height()+'px'
+		'line-height': $ensoleillement.height() + 'px'
 	});
 
 });
