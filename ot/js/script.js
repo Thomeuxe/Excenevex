7655c780e011e5893fc8564741d3c189063c9fe5
Michkkael <Michkkael@gmail.com>
2015-02-24 13:05:17 +0100

    Desktop menu : Animation on hover fix
    Animation on hover/unhover is now calculated automatically

--- ot/js/script.js
+++ ot/js/script.js
@@ -42,14 +42,45 @@ jQuery(document).ready(function ($) {
 			});
 	// end of fix
 	
-	//à mettre en js pour centrer le .submenu
-	//.submenu li a {
-	//pour 1 item : left: 45px;
-	//pour 2 items : left: 10px;
-	//pour 3 items : left: -20px;
-	//pour 4 items : left: -50px;
-	//pour 5 items : left: -88px;
-	//}
+	// Adding animation for .submenu for desktop
+		if (window.matchMedia("(min-width: 768px)").matches) {
+			$('#responsiveMenu ul li').each(function() {
+				$(this).find('.submenu li').each(function(index) {
+					//when mouse hover
+						$(this).find('a').addClass('transition-delay' + index);
+					//when mouse unhover
+						delay = 5; //time between each "a"
+						delay2 = 20 //time between unhover instant and the start of animation
+						timer = (delay*index + delay2)/100; 
+						$(this).find('a').css('transition-delay', timer + 's');
+				});
+			});
+		}
+		$(window).resize(function(event) {
+			if (window.matchMedia("(min-width: 768px)").matches) {
+				$('#responsiveMenu ul li').each(function() {
+					$(this).find('.submenu li').each(function(index) {
+						//when mouse hover
+							$(this).find('a').addClass('transition-delay' + index);
+						//when mouse unhover
+							delay = 5; //time between each "a"
+							delay2 = 20 //time between unhover instant and the start of animation
+							timer = (delay*index + delay2)/100; 
+							$(this).find('a').css('transition-delay', timer + 's');
+					});
+				});
+			}
+		});
+	// end
+	
+	// Method to center the .submenu compared to responsiveMenu for desktop
+		if (window.matchMedia("(min-width: 768px)").matches) {
+				//.submenu {
+				//	margin-left: Int((-230/7)*x+545/7));
+				//}
+		}
+	// end of center method
+	
 // end of ResponsiveMenu
 	
 // Caculate size of bubbles
