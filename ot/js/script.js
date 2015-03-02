4386373d52097feb8ab9712ac8bbffea62f7495b
Michkkael <Michkkael@gmail.com>
2015-03-02 11:52:06 +0100

    Maximage implemented

--- ot/js/script.js
+++ ot/js/script.js
@@ -1,4 +1,21 @@
 jQuery(document).ready(function ($) {
+//Maximage2 carousel
+	$(function(){
+		$('#maximage').maximage({
+			cycleOptions: {
+				fx:'scrollHorz',
+				speed: 800,
+				timeout: 8000,
+				prev: '#arrow_left',
+				next: '#arrow_right'
+			},
+			onFirstImageLoaded: function(){
+				jQuery('#cycle-loader').hide();
+				jQuery('#maximage').fadeIn('fast');
+			}
+		});
+	});
+	
 // ResponsiveMenu
 	$('#responsiveMenu>ul').hide();
 
